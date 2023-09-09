1f13c25491ac904b3ce6f013934ff89acd529384
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-09-09 10:27:26 +0530

    working on the bulb

--- src/components/LightBulb.jsx
+++ src/components/LightBulb.jsx
@@ -4,7 +4,7 @@ const LightBulb = ({ isOn }) => {
   return (
     <div className="light-bulb">
       <img
-        src={isOn ? './images/bulb_on' : './images/bulb_off'}
+        src={isOn ? '/images/bulb_on.jpg' : '/images/bulb_off.jpg'}
         alt={isOn ? 'Bulb On' : 'Bulb Off'}
       />
     </div>
