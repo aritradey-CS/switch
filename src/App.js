a2bf685ecde5f4aa80c851da398ec64dcd025431
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-09-09 11:20:13 +0530

    working on the design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { useState } from 'react'
-import './App.css'
-import on from './images/on.png'
-import off from './images/off.png'
+import React, { useState } from 'react';
+import './App.css';
+import on from './images/on.png';
+import off from './images/off.png';
 
 const App = () => {
-  const [toggle, settoggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
 
-  const togglebutton = ()=>{
-    settoggle(!toggle)
-  }
+  const toggleButton = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <div align="center">
-
-      <img src={toggle?on:off} className='bulb' alt='' />
-      <button className='button' onClick={togglebutton} >{toggle?'on':'off'}</button>
-      
+    <div className="container">
+      <div className="bulb-container">
+        <img src={toggle ? on : off} className="bulb" alt="" />
+      </div>
+      <button className="button" onClick={toggleButton}>
+        {toggle ? 'Turn Off' : 'Turn On'}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
